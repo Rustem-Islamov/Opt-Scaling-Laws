e89a6b82e0afae48e5484313fa44f80590e43702
SanyaGaponov <agaponov04@gmail.com>
2025-09-24 11:43:37 +0300

    heterogenious dataset

--- src/dataloaders/prep_data.py
+++ src/dataloaders/prep_data.py
@@ -4,7 +4,12 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, Subset
 
 
-def create_loaders(dataset_name, n_workers, batch_size, seed=42):
+def create_loaders(dataset_name, 
+                   n_workers, 
+                   batch_size,
+                   val_ratio = 0.001,
+                   homogenity_coef = 2,
+                   ):
 
     train_data, test_data = load_data(dataset_name)
 
@@ -12,26 +17,24 @@ def create_loaders(dataset_name, n_workers, batch_size, seed=42):
     n = len(train_data)
 
     # preparing iterators for workers and validation set
-    np.random.seed(seed)
+    #np.random.seed(seed)
     indices = np.arange(n)
     np.random.shuffle(indices)
 
-    n_val = int(np.floor(0.001 * n))
+    n_val = int(np.floor(val_ratio * n))
+
     val_data = Subset(train_data, indices=indices[:n_val])
 
     indices = indices[n_val:]
     n = len(indices)
-    a = int(np.floor(n / (2*n_workers)))
+    a = int(np.floor(n / (homogenity_coef * n_workers)))
     top_ind = a * n_workers
-    #top_ind = 0
     seq = range(a, top_ind, a)
-    split = np.split(indices[:top_ind], seq)
-    #split = [np.array([]).astype(int) for _ in range(n_workers)]
-    
-    
-    for idx in indices[top_ind:]:
-    #for idx in indices:
-        cur_cl = train_data[idx][1]
+
+    split = np.split(indices[:top_ind], seq) #split prefix equally
+
+    for idx in indices[top_ind:]:           #add heterogenity for the suffix
+        cur_cl = train_data[idx][1] % n_workers
         split[cur_cl] = np.append(split[cur_cl], idx)
         
     min_len = min([len(split[i]) for i in range(n_workers)])
@@ -45,7 +48,7 @@ def create_loaders(dataset_name, n_workers, batch_size, seed=42):
         train_loader_workers[b] = DataLoader(Subset(train_data, ind), batch_size=batch_size, shuffle=True)
         b = b + 1
 
-    test_loader = DataLoader(test_data, batch_size=1000, shuffle=False)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
     val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
 
     return train_loader_workers, val_loader, test_loader

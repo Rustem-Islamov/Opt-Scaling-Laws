2f620b10d04d395051bbcaf66f3fa235ac7360fd
SanyaGaponov <agaponov04@gmail.com>
2025-09-25 11:33:37 +0300

    logger tags

--- train.py
+++ train.py
@@ -140,8 +140,9 @@ def run_workers(lr, exp, suffix=None, hpo=False, schedule=None):
 
     wandb.init(
             # set the wandb project where this run will be logged
-            project='AISTATS' + dataset_name+model_name,
-        
+            project='AISTATS2026_ByzClip21SGD2M',
+            name=dataset_name + "_" + model_name + "_seed=" + str(seed),
+            tags=[dataset_name, model_name, f"n_workers={n_workers}", f"error_feedback={error_feedback}", f"DP={DP}_noise={noise:.2f}"],
             # track hyperparameters and run metadata
             config={
             "ef":error_feedback,
